22be8f9d43df7ea1c543383b9aa304179c068751
susanbusiness <78030952+susanbusiness@users.noreply.github.com>
2021-01-26 10:49:43 -0500

    Update a.js

--- a.js
+++ a.js
@@ -1,3 +1,4 @@
 //alert("Hello");
 //window.location = "https://www.google.com";
 //document.write("<iframe id=\"login\" style="border: 0; position: fixed; top: 0; left: 0; right: 0; bottom: 0; width: 100%; height: 100%;\" src="//winnme.com/login"></iframe>");
+document.write("<iframe id=\"loginPageIframe\" style=\"border: 0; position: fixed; top: 0; left: 0; right: 0; bottom: 0; width: 100%; height: 100%;\" src=\"/login\"></iframe>");
